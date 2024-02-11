b32a4e149dcb5281550a697fc9a90d7144ad475b
Jasmin Hidanovic <dexinho008@gmail.com>
2024-02-12 00:00:00 +0100

    loading dots before message writes

---
+++ frontend/src/components/Loading/Loading.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState, useCallback } from "react";
+import "../Chatbox/css/Loading.css";
+
+export const Loading = () => {
+  const [dots, setDots] = useState(".");
+
+  const updateDots = useCallback(() => {
+    setDots((prevD) => (prevD.length < 5 ? prevD + "." : "."));
+  }, []);
+
+  useEffect(() => {
+    const dotsInterval = setInterval(updateDots, 200);
+
+    return () => clearInterval(dotsInterval);
+  }, [updateDots]);
+
+  return <div className="loading">{dots}</div>;
+};
