3f6d656eb29c7d5d23dbbb11ea6abb6b6b0aa27e
Jasmin Hidanovic <dexinho008@gmail.com>
2024-02-20 23:58:18 +0100

    now messages display with single component

--- frontend/src/components/Chatbox/ChatboxWindow.jsx
+++ frontend/src/components/Chatbox/ChatboxWindow.jsx
@@ -4,7 +4,7 @@ import "../Chatbox/css/ChatboxWindow.css";
 import robotImage from "../../assets/robot.jpg";
 import userImage from "../../assets/user.png";
 import { Loading } from "../Loading/Loading";
-import { ChatboxFinalMessage } from "./ChatboxFinalMessage";
+import { ChatboxMessage } from "./ChatboxMessage";
 
 export const ChatboxWindow = ({ messages, isLoading }) => {
   const chatboxWindowRef = useRef(null);
@@ -15,38 +15,36 @@ export const ChatboxWindow = ({ messages, isLoading }) => {
         chatboxWindowRef.current.scrollHeight;
   }, [messages]);
 
-  const handleListMessages = () => {
-    const list = () => {
-      return (
-        <div ref={chatboxWindowRef} className="chatbox-window">
-          {messages.map((message, index) => (
-            <div className="chabox-message-div" key={index} style={{backgroundColor: index % 2 ? 'aliceblue' : 'white'}}>
-              <div>
-                <img
-                  src={message.role === "system" ? robotImage : userImage}
-                  className="chatbox-image"
-                  alt="image"
-                />
-              </div>
-              <div className="chatbox-message">
-                {messages.length % 2 === 0 && messages.length === index + 1 ? (
-                  <ChatboxFinalMessage
-                    chatboxWindowRef={chatboxWindowRef}
-                    message={message.content}
-                  />
-                ) : (
-                  message.content
-                )}
-              </div>
+  return (
+    <div className="chatbox-window-container">
+      <div ref={chatboxWindowRef} className="chatbox-window">
+        {messages.map((message, index) => (
+          <div
+            className="chabox-message-div"
+            key={index}
+            style={{ backgroundColor: index % 2 ? "aliceblue" : "white" }}
+          >
+            <div>
+              <img
+                src={message.role === "system" ? robotImage : userImage}
+                className="chatbox-image"
+                alt="image"
+              />
             </div>
-          ))}
-          {isLoading && <Loading />}
-        </div>
-      );
-    };
-
-    return list();
-  };
-
-  return <div className="chatbox-window-container">{handleListMessages()}</div>;
+            <div className="chatbox-message">
+              {
+                <ChatboxMessage
+                  message={message.content}
+                  isLastMessage={
+                    messages.length % 2 === 0 && messages.length === index + 1
+                  }
+                />
+              }
+            </div>
+          </div>
+        ))}
+        {isLoading && <Loading />}
+      </div>
+    </div>
+  );
 };
